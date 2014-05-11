54877098153acedcdaf4f8b1087e3f04a056a7ac
JamesSullivan1 <calgaryjames@gmail.com>
2014-05-11 10:14:04 -0600

    Added user prompts

--- game.c
+++ game.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
 
 #define CELL(I,J) (board[size*(I) + (J)])
 #define ALIVE(I,J) t[h*(I) + (J)] = 1
@@ -11,6 +12,8 @@
 
 #define DEAD_STR "\033[07m \033[m"
 #define ALIVE_STR "  "
+
+#define BUF_SIZE 10
 
 /*
  * Returnst he number of cells alive on the board.
@@ -82,46 +85,94 @@ int num_adjacent(void *u, int w, int h, int x, int y)
  */
 void evolve(void *u, int w, int h)
 {
+    if(w < 0 || h < 0) return;
+
     unsigned (*univ)[w] = u;
     unsigned new[h][w];
 
     int x, y;
     for_y for_x {
         int n = num_adjacent(u, w, h, x, y);
-        // If the cell has 3 or more adjacencies, enable.
-        //  Or if the cell has 2 adjacencies and is itself enabled, enable.
         new[y][x] = (n == 3 || (n == 2 && univ[y][x])); 
     } 
     for_y for_x univ[y][x] = new[y][x];
 }
 
 /*
- * Game loop.
+ * Game loop. (w,h) give board dimensions, s gives speed, n is max steps.
  *  1) Display state
  *  2) Update state
  */
-void game(int w, int h)
+void game(int w, int h, int s, int n)
 {
     unsigned univ[h][w];
-    // Random board setup
     int x, y;
     int step = 0;
+    int sleep_time = (6 - s)*50000;
+    // Random initialization
     for_x for_y univ[y][x] = rand() < RAND_MAX / 10 ? 1 : 0; 
-    while (1) {
+    while (1 || (step < n && n > 0)) {
         display(univ, w, h, step++);
         evolve(univ, w, h);
-        usleep(100000);
+        usleep(sleep_time);
     }
+
+    fflush(stdout);
+    printf("Simulation ended after %d steps\n",step);
+}
+
+/*
+ * Gets an integer input of size < BUF_SIZE from STDIN.
+ *  Can give an upper and lower bound, default value, display message and default message.
+ */
+int get_option(int min, int max, int default_val, char *message, char *default_message)
+{
+    char in[BUF_SIZE];
+    int val;
+
+    printf("%s: ", message);
+    fgets(in, BUF_SIZE + 1, stdin);
+    val = atoi(in);
+    if(!val || val < min || val > max) {
+        printf("%s\n", default_message);
+        val = default_val;
+    }
+
+    return val;
+
 }
 
 int main(int argc, char *argv[])
 {
-        int w, h = 0;
-        if(argc > 1) w = atoi(argv[1]);
-        if(argc > 2) h = atoi(argv[2]);
-        if(w <= 0) w = 30;
-        if(h <= 0) h = 30; // Defaults
-        game(w,h);
+    if(argc > 1) {
+        printf("USAGE: ./game");
+        return 1;
+    }
 
-        return 0;
+    int w, h, s, n;
+
+    printf("-----CONWAY'S GAME OF LIFE-----\n");
+    printf("Press CTRL + C to end game.\n");
+
+    char *message = "Enter the board width (0 < w < 100)";
+    char *default_message = "Setting to default value of 30";
+    w = get_option(1, 99, 30, message, default_message);
+
+    message = "Enter the board height (0 < h < 100)";
+    h = get_option(1, 99, 30, message, default_message);
+
+    message = "Enter the game speed (1 ... 5)";
+    default_message = "Setting to default value of 3";
+    s = get_option(1, 5, 3, message, default_message);
+
+    message = "Enter the max number of steps (0 for no max)";
+    default_message = "Unlimited number of steps";
+    n = get_option(0, INT_MAX, 0, message, default_message); 
+
+    printf("-----Beginning Simulation-----");
+    sleep(1);
+    printf("\033[2J");
+
+    game(w,h,s,n);
+    return 0;
 }
